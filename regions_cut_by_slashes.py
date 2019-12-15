2f13aac6f5338661a962d88d3d3ac36d94c5910c
ra5ika27 <31758298+ra5ika27@users.noreply.github.com>
2019-12-14 20:35:13 -0500

    Regions cut by slashes

--- regions_cut_by_slashes.py
+++ regions_cut_by_slashes.py
@@ -1,6 +1,6 @@
 class DSU(object):
     def __init__(self, N):
-        self.parent = [ i for i in range(N)]
+        self.parent = [i for i in range(N)]
 
     def find(self, x):
         while self.parent[x] != x:
@@ -11,3 +11,32 @@ class DSU(object):
         unique_x = self.find(x)
         unique_y = self.find(y)
         self.parent[unique_x] = unique_y
+
+
+class Solution(object):
+    def regionsBySlashes(self, grid):
+        N = len(grid)
+        dsu = DSU(4 * N * N)
+
+        for r, row in enumerate(grid):
+            for c, val in enumerate(row):
+                root = 4 * (r * N + c)
+                if val in '/ ':
+                    dsu.union(root + 0, root + 1)
+                    dsu.union(root + 2, root + 3)
+                if val in '\ ':
+                    dsu.union(root + 0, root + 2)
+                    dsu.union(root + 1, root + 3)
+
+                # for top and bottom
+                if r + 1 < N:
+                    dsu.union(root + 3, (root + 4 * N) + 0)
+                if r - 1 >= 0:
+                    dsu.union(root + 0, (root - 4 * N) + 3)
+                # for left and right
+                if c + 1 < N:
+                    dsu.union(root + 2, (root + 4) + 1)
+                if c - 1 >= 0:
+                    dsu.union(root + 1, (root - 4) + 2)
+
+        return sum(dsu.find(x) == x for x in range(4 * N * N))
